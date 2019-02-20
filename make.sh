126d846336bbf6d65cf3b5c175fb78055363517c
svoboda18 <ruspam18@gmail.com>
2019-02-20 11:56:42 +0000

    configs: p1m: add oreo deps

---
+++ make.sh
@@ -0,0 +1,9 @@
+export KBUILD_BUILD_USER="Mohancm"
+export KBUILD_BUILD_HOST="TEAM_ZERO"
+export CROSS_COMPILE="/home/easycompile2019/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
+make clean && make mrproper
+
+mkdir out
+
+make ARCH=arm64 O=out p1m_defconfig;make ARCH=arm64 -j30 O=out ;
