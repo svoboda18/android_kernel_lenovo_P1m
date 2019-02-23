883ba20f1b7aa3ac9e1386c7a27d802a5813f3e2
svoboda18 <ruspam18@gmail.com>
2019-02-23 13:48:59 +0000

    drivers: mediatek: connectivity: update bluetooth driver to 2.0

--- make.sh
+++ make.sh
@@ -2,8 +2,10 @@ export KBUILD_BUILD_USER="Mohancm"
 export KBUILD_BUILD_HOST="TEAM_ZERO"
 export CROSS_COMPILE="/home/easycompile2019/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 
+read -p "Do you want to clean" clean
+[ $clean = "y" ] && {
 make clean && make mrproper
-
 mkdir out
+}
 
 make ARCH=arm64 O=out p1m_defconfig;make ARCH=arm64 -j30 O=out ;
